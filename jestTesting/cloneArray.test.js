6be003b32c89f9b6d24bbdbb5a83d5fa1883a129
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-04-13 13:45:35 -0500

    added notes about the testing.

--- jestTesting/cloneArray.test.js
+++ jestTesting/cloneArray.test.js
@@ -2,5 +2,7 @@ const { test, expect } = require('@jest/globals');
 const cloneArray = require('./cloneArray');
 
 test('Clones an array', () => {
-    expect(cloneArray([1, 3, 11])).toStrictEqual([1, 3, 11])
+    // .toEqual is required here instead of .toBe because although they are "the same" 
+    // they are stored in different memory files. .toEqual checks if the value is the same.
+    expect(cloneArray([1, 3, 11])).toEqual([1, 3, 11])
 })
--- jestTesting/sum.test.js
+++ jestTesting/sum.test.js
@@ -1,5 +1,7 @@
 // import { test } from '@jest/globals';
 // import sum from './sum';
+
+// tried to use the ES6 syntax and it throws an error. Must be written with require.
 const sum = require('./sum');
 
 test('properly adds two numbers.', () => {
