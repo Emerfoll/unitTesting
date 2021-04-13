e67bacf908e23f43975f08b246325b49ed66feb7
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-04-13 13:39:01 -0500

    subtract test written and passes first test.

--- jestTesting/cloneArray.test.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@jest/globals');
-const cloneArray = require('./cloneArray');
-
-test('Clones an array', () => {
-    expect(cloneArray([1, 3, 11])).toStrictEqual([1, 3, 11])
-})
