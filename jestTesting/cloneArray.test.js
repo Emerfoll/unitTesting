fea943e68855a019f170a0a26806ddff0d055ddd
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-04-13 13:58:13 -0500

    added --coverage and an untested function.

--- jestTesting/cloneArray.test.js
+++ jestTesting/cloneArray.test.js
@@ -2,7 +2,11 @@ const { test, expect } = require('@jest/globals');
 const cloneArray = require('./cloneArray');
 
 test('Clones an array', () => {
+    array = [1, 3, 11]
     // .toEqual is required here instead of .toBe because although they are "the same" 
     // they are stored in different memory files. .toEqual checks if the value is the same.
-    expect(cloneArray([1, 3, 11])).toEqual([1, 3, 11])
+    expect(cloneArray(array)).toEqual(array)
+    // this test makes sure that the returned array is a clone of the original array and
+    // the test didn't just return the original array.
+    expect(cloneArray(array)).not.toBe(array)
 })
--- jestTesting/sum.js
+++ jestTesting/sum.js
@@ -2,4 +2,10 @@ function sum(a, b) {
     return a + b;
 }
 
+// since this function is not tested, when the test is ran it will still pass all tests
+// but, if we are using --coverage, we will be told this function did not run
+function untestedFunction(thing) {
+    return thing;
+}
+
 module.exports = sum;
